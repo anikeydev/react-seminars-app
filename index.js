6e7b66db60ee38a50d7f680ca0c80cb596d5e5c0
anikey vladislav <dev.anikey.va@mail.ru>
2025-03-19 18:24:25 +0300

    update photo

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ let seminars = [
     description: 'Обзор новых средств и методик от Kosmoteros.',
     date: '01.02.2025',
     time: '10:00',
-    photo: 'https://picsum.photos/id/1/750/730',
+    photo:
+      'https://avatars.mds.yandex.net/i?id=992a0bb22c4efe6e8ac3f2d12747b4fc_l-5656190-images-thumbs&n=13',
   },
   {
     id: 2,
@@ -22,7 +23,8 @@ let seminars = [
       'Разбор новейших тенденций в косметологии и трендовых продуктов.',
     date: '03.02.2025',
     time: '11:00',
-    photo: 'https://picsum.photos/id/2/750/730',
+    photo:
+      'https://avatars.mds.yandex.net/i?id=c18d23e9bf25a6833bd47528e203f699_l-8496938-images-thumbs&n=13',
   },
   {
     id: 3,
@@ -31,7 +33,7 @@ let seminars = [
       'Изучение современных методик ухода за кожей с применением новейших технологий.',
     date: '05.02.2025',
     time: '12:00',
-    photo: 'https://picsum.photos/id/3/750/730',
+    photo: 'https://cdn.culture.ru/images/2bedcf8b-a9da-56f9-82c4-edddfe41e6ef',
   },
   {
     id: 4,
@@ -39,7 +41,8 @@ let seminars = [
     description: 'Обсуждение современных трендов и методик в бьюти-индустрии.',
     date: '07.02.2025',
     time: '13:00',
-    photo: 'https://picsum.photos/id/4/750/730',
+    photo:
+      'https://avatars.mds.yandex.net/get-altay/4544819/2a00000179e2cba3089fa1720e4845b2ad3d/XXL_height',
   },
   {
     id: 5,
@@ -48,7 +51,7 @@ let seminars = [
       'Семинар с участием ведущих специалистов по использованию средств Kosmoteros.',
     date: '09.02.2025',
     time: '14:00',
-    photo: 'https://picsum.photos/id/5/750/730',
+    photo: 'https://cdn.culture.ru/images/2bedcf8b-a9da-56f9-82c4-edddfe41e6ef',
   },
   {
     id: 6,
@@ -56,7 +59,7 @@ let seminars = [
     description: 'Обучение современным методикам омоложения и ухода за кожей.',
     date: '11.02.2025',
     time: '15:00',
-    photo: 'https://picsum.photos/id/6/750/730',
+    photo: 'https://cdn.culture.ru/images/2bedcf8b-a9da-56f9-82c4-edddfe41e6ef',
   },
   {
     id: 7,
@@ -65,7 +68,8 @@ let seminars = [
       'Демонстрация новых продуктов Kosmoteros и их уникальных свойств.',
     date: '13.02.2025',
     time: '16:00',
-    photo: 'https://picsum.photos/id/7/750/730',
+    photo:
+      'https://avatars.mds.yandex.net/i?id=c18d23e9bf25a6833bd47528e203f699_l-8496938-images-thumbs&n=13',
   },
   {
     id: 8,
@@ -74,7 +78,8 @@ let seminars = [
       'Семинар о новейших трендах в уходе за кожей и эффективных методах лечения.',
     date: '15.02.2025',
     time: '17:00',
-    photo: 'https://picsum.photos/id/8/750/730',
+    photo:
+      'https://avatars.mds.yandex.net/i?id=c18d23e9bf25a6833bd47528e203f699_l-8496938-images-thumbs&n=13',
   },
   {
     id: 9,
@@ -83,7 +88,8 @@ let seminars = [
       'Практический мастер-класс по использованию инновационных косметических средств.',
     date: '17.02.2025',
     time: '18:00',
-    photo: 'https://picsum.photos/id/9/750/730',
+    photo:
+      'https://avatars.mds.yandex.net/i?id=992a0bb22c4efe6e8ac3f2d12747b4fc_l-5656190-images-thumbs&n=13',
   },
   {
     id: 10,
@@ -92,7 +98,8 @@ let seminars = [
       'Обсуждение перспектив развития косметологии и роли инноваций.',
     date: '19.02.2025',
     time: '19:00',
-    photo: 'https://picsum.photos/id/10/750/730',
+    photo:
+      'https://avatars.mds.yandex.net/i?id=992a0bb22c4efe6e8ac3f2d12747b4fc_l-5656190-images-thumbs&n=13',
   },
 ]
 //Используем окружение
